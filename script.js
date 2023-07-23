80d07ee7471d7b4aeb894df71dd7a946e5820ee2
Anish Nigam <117471360+nigam85@users.noreply.github.com>
2023-07-23 14:24:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 //your JS code here. If required.
+function updateTimer() {
+  const timerElement = document.getElementById('timer');
+  const currentDate = new Date();
+
+  const options = {
+    weekday: 'long',
+    year: 'numeric',
+    month: 'long',
+    day: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric',
+    second: 'numeric',
+  };
+
+  const formattedDate = currentDate.toLocaleDateString(undefined, options);
+
+  timerElement.textContent = formattedDate;
+}
+
+// Update the timer every second
+setInterval(updateTimer, 1000);
